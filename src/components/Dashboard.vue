be5f1ec65ca5a40b0043d5d0e366c38b2a94037f
Lassi <lassi.palojarvi@outlook.com>
2022-06-16 23:03:06 +0300

    App init

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
